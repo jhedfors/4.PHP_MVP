b4a0b38aff8ffbd80b5776ee84202e31a308e00f
Jeff Hedfors <git.j@hedfors.net>
2016-04-19 10:30:57 -0700

    updated with ORM

--- 6.Courses/application/models/Course.php
+++ 6.Courses/application/models/Course.php
@@ -4,7 +4,10 @@ class Course extends CI_Model {
   //the get_all_courses method queries the database, then returns the resorted results using the array_multisort built-in function which requires that a temporary array with the sorting key be created.
 	public function get_all_courses()
 	{
-    $courses = $this->db->query("SELECT * FROM courses")->result_array();
+    // $courses = $this->db->query("SELECT * FROM courses")->result_array();
+		//Changed to ORM below
+		$courses = $this->db->get('courses')->result_array();
+
     foreach ($courses as $key => $row) {
       $sortingkey[$key] = $row['created_at'];
     }
@@ -14,18 +17,28 @@ class Course extends CI_Model {
   //an insert query with the course information is submitted to the database
   function add_course($course)
   {
-    $query = "INSERT INTO Courses (name, description, created_at, modified_at) VALUES (?,?,?,?)";
-    $values = array($course['name'], $course['description'], date("Y-m-d, H:i:s"),date("Y-m-d, H:i:s"));
-    return $this->db->query($query, $values);
+    // $query = "INSERT INTO Courses (name, description, created_at, modified_at) VALUES (?,?,?,?)";
+    // $values = array($course['name'], $course['description'], date("Y-m-d, H:i:s"),date("Y-m-d, H:i:s"));
+    // return $this->db->query($query, $values);
+		//Changed to ORM below
+    $values = array('name' => $course['name'], 'description' =>$course['description'], 'created_at' => date("Y-m-d, H:i:s"),'modified_at' =>date("Y-m-d, H:i:s"));
+
+		$this->db->insert('courses', $values);
+
   }
   // this function queries and returns the information for the specific course id
   function display_course($id){
-    $query = "SELECT * FROM courses WHERE id=?";
-    return $this->db->query($query, array($id))->row_array();
+    // $query = "SELECT * FROM courses WHERE id=?";
+    // return $this->db->query($query, array($id))->row_array();
+		//Changed to ORM below
+		return  $this->db->get_where('courses', array('id' => $id))->row_array();;
   }
   //using the course ID, a query is submitted to the database to delete the record
   function delete_course($id){
-    $query = "DELETE FROM courses WHERE id=?";
-    return $this->db->query($query, array($id));
+    // $query = "DELETE FROM courses WHERE id=?";
+    // return $this->db->query($query, array($id));
+		//Changed to ORM below
+		$this->db->delete('courses', array('id' => $id));
+
   }
 }
